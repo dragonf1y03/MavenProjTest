3d64b3cf4c5678cc23ead62b1e7a3f7127b4c0c6
Robert Gatt <robert.gatt@rs2.com>
2015-04-01 11:37:48 +0200

    Added javadocs

--- src/main/java/rs2/com/maven/MavenProject/App.java
+++ src/main/java/rs2/com/maven/MavenProject/App.java
@@ -1,13 +1,21 @@
 package rs2.com.maven.MavenProject;
 
 /**
- * Hello world!
+ * Simple App for a Maven project.
  *
  */
 public class App 
 {
+	
+	/**
+	 * This method is used to sum 2 numbers.
+	 * @param x The 1st number to sum.
+	 * @param y The 2nd number to sum.
+	 * @return The summed up number of x and y.
+	 */
     public int summation(int x, int y)
     {
     	return x + y;
     }
+    
 }
